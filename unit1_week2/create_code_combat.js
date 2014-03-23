352e89e86a7da7a3fb894ab4cbb994e6dcc74e5d
Eiko Seino <eikoseino@hotmail.com>
2014-03-23 15:45:53 -0700

    code combat

---
+++ unit1_week2/create_code_combat.js
@@ -0,0 +1,22 @@
+var tharin = { name: "Tharin", weapon: "arrow"};
+var ogre1 = { name: "Ogre1", weapon: "sword"};
+var ogre2 = { name: "Ogre2", weapon: "sword"};
+var ogre3 = { name: "Ogre3", weapon: "sword"};
+
+ogre1.chaseTharin();
+tharin.move();
+ogre1.chaseTharin();
+tharin.climbUp();
+tharin.shootArrow();
+
+ogre2.chaseTharin();
+tharin.move();
+ogre2.chaseTharin();
+tharin.climbUp();
+tharin.shootArrow();
+
+ogre3.chaseTharin();
+tharin.move();
+ogre3.chaseTharin();
+tharin.climbUp();
+tharin.shootArrow();
